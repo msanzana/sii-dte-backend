4a98e7a699ad133128f111eaee7d92b8dceb248f
msanzana <33297167+msanzana@users.noreply.github.com>
2026-07-01 11:06:36 -0400

    BIndings de DTE

--- app/Providers/DteRepositoryServiceProvider.php
+++ app/Providers/DteRepositoryServiceProvider.php
@@ -3,23 +3,25 @@
 namespace App\Providers;
 
 use App\Modules\Dte\Domain\RepositoryContracts\CityRepositoryInterface;
+use App\Modules\Dte\Domain\RepositoryContracts\CompanyCertificateNoticeRepositoryInterface;
 use App\Modules\Dte\Domain\RepositoryContracts\CompanyRepositoryInterface;
 use App\Modules\Dte\Domain\RepositoryContracts\DteDocumentRepositoryInterface;
 use App\Modules\Dte\Domain\RepositoryContracts\IntegrationLogRepositoryInterface;
 use App\Modules\Dte\Domain\RepositoryContracts\LocationRepositoryInterface;
 use App\Modules\Dte\Domain\RepositoryContracts\SiiCafRepositoryInterface;
 use App\Modules\Dte\Domain\RepositoryContracts\SiiCertificateRepositoryInterface;
+use App\Modules\Dte\Domain\RepositoryContracts\SiiDispatchRepositoryInterface;
 use App\Modules\Dte\Domain\RepositoryContracts\SystemSettingRepositoryInterface;
 use App\Modules\Dte\Infrastructure\Persistence\Repositories\EloquentCityRepository;
+use App\Modules\Dte\Infrastructure\Persistence\Repositories\EloquentCompanyCertificateNoticeRepository;
 use App\Modules\Dte\Infrastructure\Persistence\Repositories\EloquentCompanyRepository;
 use App\Modules\Dte\Infrastructure\Persistence\Repositories\EloquentDteDocumentRepository;
 use App\Modules\Dte\Infrastructure\Persistence\Repositories\EloquentIntegrationLogRepository;
 use App\Modules\Dte\Infrastructure\Persistence\Repositories\EloquentLocationRepository;
 use App\Modules\Dte\Infrastructure\Persistence\Repositories\EloquentSiiCafRepository;
 use App\Modules\Dte\Infrastructure\Persistence\Repositories\EloquentSiiCertificateRepository;
-use App\Modules\Dte\Infrastructure\Persistence\Repositories\EloquentSystemSettingRepository;
-use App\Modules\Dte\Domain\RepositoryContracts\SiiDispatchRepositoryInterface;
 use App\Modules\Dte\Infrastructure\Persistence\Repositories\EloquentSiiDispatchRepository;
+use App\Modules\Dte\Infrastructure\Persistence\Repositories\EloquentSystemSettingRepository;
 use Illuminate\Support\ServiceProvider;
 
 class DteRepositoryServiceProvider extends ServiceProvider
@@ -68,6 +70,11 @@ class DteRepositoryServiceProvider extends ServiceProvider
         $this->app->bind(
             SiiDispatchRepositoryInterface::class,
             EloquentSiiDispatchRepository::class
+        );
+
+        $this->app->bind(
+            CompanyCertificateNoticeRepositoryInterface::class,
+            EloquentCompanyCertificateNoticeRepository::class
         );
     }
 
